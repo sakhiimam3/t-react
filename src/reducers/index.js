478d5d1e9da2e573a1b423c2029e92ea074857f8
sakhiimam3 <sakhiimam3411@gmail.com>
2021-01-17 22:37:49 +0500

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers  } from "redux";
+import  { todos } from "../reducers/todos"
+
+
+
+
+const rootReducer= combineReducers({   
+    todos,
+
+});
+
+ export default rootReducer;
